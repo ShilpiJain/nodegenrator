2da7102caf5249ed982972bb0eb71be42fb8f7a1
Shilpi Jain <>
2019-05-31 13:29:12 +0530

    first commit

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+// Full Documentation - https://www.turbo360.co/docs
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+/*  This is a sample API route. */
+
+router.get('/:resource', (req, res) => {
+	res.json({
+		confirmation: 'success',
+		resource: req.params.resource,
+		query: req.query // from the url query string
+	})
+})
+
+router.get('/:resource/:id', (req, res) => {
+	res.json({
+		confirmation: 'success',
+		resource: req.params.resource,
+		id: req.params.id,
+		query: req.query // from the url query string
+	})
+})
+
+
+
+module.exports = router
